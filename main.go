d032a1f535b6f8560d79aa33461c8926b576f2a7
Den <weblifeon@gmail.com>
2022-10-20 14:07:11 +0300

    last

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
 
-func main() {
+func metrics(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "alive 1\n")
+}
 
-	for {
-		http.Get("https://questrequest.ru/cron/cron_amo_buy_sell_search_infopart")
-		time.Sleep(3 * time.Second)
+func main() {
+	go func() {
+		for {
+			http.Get("https://questrequest.ru/cron/cron_amo_buy_sell_search_infopart")
+			time.Sleep(3 * time.Second)
+		}
+	}()
+
+	http.HandleFunc("/metrics", metrics)
+
+	log.Println("Starting webserver...")
+	err := http.ListenAndServe(":9999", nil)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 }
